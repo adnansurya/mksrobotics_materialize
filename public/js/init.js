e37e8afff09f469001aa5a8bc51aeaddb3c13138
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-02-28 22:12:32 +0800

    materialize element init

--- public/js/init.js
+++ public/js/init.js
@@ -4,32 +4,93 @@ $( document ).ready(function() {
   console.log( "ready!" );
 
   $('.sidenav').sidenav();
-   
-  
+  $('select').formSelect();
+  $('.fixed-action-btn').floatingActionButton();
+  $('.modal').modal();
+
 
   const db = firebase.database()
+  let totalProduct, totalPage, lastUpdated;
+
+  loadProperties(); 
 
   db.ref('category').once('value').then(function(snapshot){
+    $('.categoryDiv').append(
+      `<p style="margin-top: 5px;margin-bottom: 5px;">
+        <label>
+          <input class="with-gap" name="category" type="radio" value="SEMUA" checked/>
+          <span style="padding-left: 25px;">SEMUA KATEGORI</span>
+        </label>
+      </p>`
+    );
     snapshot.forEach(function(oneSnap){
       let data = oneSnap.val()
-      $('#categoryDiv').append(
-        `<p>
+      $('.categoryDiv').append(
+        `<p style="margin-top: 5px;margin-bottom: 5px;">
           <label>
-            <input class="with-gap" name="category" type="radio" />
-            <span>` +  data.name + `</span>
+            <input class="with-gap" name="category" type="radio" value="`+ data.name+`" />
+            <span class="text-blue" style="padding-left: 25px;">` +  data.name + `</span>
           </label>
         </p>`
       );
     });
     $('#categoryLoad').addClass('hide');
+    $('#webCategory input').on('change', function() {
+      let selectedCategory = $('input[name=category]:checked', '#webCategory').val()
+      loadProduct(selectedCategory, 10, 1);
+   });
   });
 
-  
-  db.ref('product_data').orderByKey().limitToFirst(20).once('value').then(function(snapshot){
-    snapshot.forEach(function(oneSnap){
-      let data = oneSnap.val() 
+  function loadProperties(){
+    db.ref('properties').once('value').then(function(snapshot){
+      let data = snapshot.val()
+      lastUpdated = data.last_sync
+      totalProduct = data.product_total        
+      $('#lastUpdateTxt').text("Update : " + lastUpdated);
+      loadProduct("SEMUA",10,1);
+    });
+  }
 
-      $('#productDiv').append(
+
+  function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+  }
+
+
+  function loadProduct(category, perPage, currentPage){
+
+    $('#productLoad').removeClass('hide');
+    $('#productDiv').empty();
+    let allSnap;
+    
+    if(category === "SEMUA"){
+      totalPage = Math.round(totalProduct / perPage);
+      if(totalProduct % perPage != 0){
+        totalPage += 1;
+      }
+      console.log(totalPage);
+      db.ref('product_data').orderByKey().startAt('10').limitToFirst(10).once('value').then(function(snapshot){    
+        allSnap = clone(snapshot.val());        
+        appendProduct(allSnap);            
+      });
+    }else{
+      db.ref('product_data').orderByChild('category').equalTo(category).once('value').then(function(snapshot){    
+        allSnap = clone(snapshot.val());        
+        appendProduct(allSnap);
+      });
+    }   
+  }
+
+  function appendProduct(all){
+
+    Object.keys(all).forEach(function(k) {
+       let data = all[k];
+       $('#productDiv').append(
         `<div class="col l3 m4 s6">
           <div class="card hoverable">
             <div class="card-image">
