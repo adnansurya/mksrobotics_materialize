055e8fbd4c8fd3f3bf8a12fea91eb199eb736931
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-03-03 21:15:27 +0800

    product page

---
+++ public/js/pages/admin.js
@@ -0,0 +1,20 @@
+ // const auth = firebase.auth();
+
+  // auth.onAuthStateChanged(function(user) {
+  //   if (user) {          
+  //       $('#tulisan').text('Hai, ' + user.displayName);
+  //   }else{
+  //     location.href = "login.html"
+  //   }
+  // });
+
+  // $('#logout_btn').on('click', function(){
+  //     auth.signOut().then(function() {                    
+  //       toast('Anda telah keluar!');
+  //       location.href = 'login.html';   
+
+  //     }).catch(function(error) {
+  //         toast(error.message);
+
+  //     });
+  // });
