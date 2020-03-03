d51ff2b4809bbc37b4c40d28ae0ebe933836ecc8
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-03-03 20:50:38 +0800

    blank ejs template

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,21 @@
 const functions = require('firebase-functions');
+const express = require('express');
+var path = require("path");
 
+
+const app = express();
+const web_name = 'Makassar Robotics';
+
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+
+
+app.get('/blank', (req,res) => {
+    res.render('pages/blank');    
+});
+
+exports.apps = functions.https.onRequest(app);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
