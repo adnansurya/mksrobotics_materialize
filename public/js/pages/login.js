1134bfc8272736b137e9e182cbd54f22295df1e4
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-03-23 22:15:18 +0800

    llogin delay redirect

--- public/js/pages/login.js
+++ public/js/pages/login.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function(){    
-
+    const auth = firebase.auth();
     $('.modal').modal(); 
     
     $('#daftarForm').on('submit', function(event){
@@ -16,13 +16,51 @@ $(document).ready(function(){
            
             if(data === 'berhasil'){
                 toast('Pendaftaran Berhasil!');
-                delayRedirect('/login', 2);
+                delayRedirect('/login', 1);
             }else if(data === 'unmatch'){
                 toast('Password Tidak Sama!');            
             }else{
                 toast(data);
             }
         });
+        
+    });
+
+    $('#loginForm').on('submit', function(event){
+        event.preventDefault();
+        var values = {};
+        $.each($('#loginForm').serializeArray(), function(i, field) {
+            values[field.name] = field.value;
+        });
+
+        auth.signInWithEmailAndPassword(values.email, values.password).catch(function(error){
+            toast(error.code + ':' +  error.message);
+        }).then(function(){
+            let user = auth.currentUser;
+            if(user){
+                user.getIdToken(true).then(function(idToken){
+                    $.post('/cek_token', {id_token: idToken})
+                    .done(function(data){
+                        if(data === 'login_success'){
+                            auth.signOut().then(function(){
+                                toast('Login Berhasil!');
+                                delayRedirect('/admin',1);
+                            }).catch(function(error){
+                                toast(error.code + ':' + error.message);
+                            });
+                        }else{
+                            toast(data);
+                        }
+                       
+                    });
+                }).catch(function(error){
+                    toast(error.code + ':' + error.message);
+                });
+            }else{
+                toast('User Tidak Ditemukan!')
+            }
+        })
+                   
         
     });
 
