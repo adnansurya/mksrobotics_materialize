4574fbaf667364ea0b3603aba0c234463592a1c7
Unknown <pc.keyboardist@gmail.com>
2020-03-24 08:26:52 +0800

    edit product pic and desc

--- public/js/pages/product.js
+++ public/js/pages/product.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     let selectedId, selectedName;
+    const db = firebase.database();
     let table = $('#product_table').DataTable( {
         "ajax": "http://localhost:5000/api/all_product",
         "columns": [
@@ -25,7 +26,7 @@ $(document).ready(function() {
             { 
                 data: 'uxid',
                 render: function ( data, type, row ) {
-                    return `<button class="waves-effect waves-light btn btn-small modal-trigger uxid" href="#modal1" data-id="`+data+`" data-name="`+row['name']+`">Edit</button>`;
+                    return `<button class="waves-effect waves-light btn btn-small modal-trigger" href="#modal1" data-id="`+data+`" data-name="`+row['name']+`">Edit</button>`;
                 } 
             }
         ],
@@ -61,12 +62,23 @@ $(document).ready(function() {
             function(){                        
                 $('#nama_text').val(selectedName);
                 $('#uxid_val').val(selectedId);
+
+                db.ref('description/'+selectedId).once('value').then(function(snapshot){
+                    let data = snapshot.val();
+                    $('#picture_text').val(data.picture);
+                    $('#details_text').val(data.details);
+                    $('#product_pic').attr("alt", data.picture);
+                    $('#product_pic').attr("src", data.picture);
+                    M.textareaAutoResize($('#picture_text'));
+                    M.textareaAutoResize($('#details_text'));
+                });
             }
     });
    
     $('#picture_text').change(function(){
         pic_url = $(this).val();              
         $('#product_pic').attr("src", pic_url);
+        $('#product_pic').attr("alt", pic_url);
         $('.materialboxed').materialbox();               
     });
     
